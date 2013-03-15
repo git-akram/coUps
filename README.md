40066ed68ebe68cf5a2a7c130e063b42aeb71ca0
Akram <20contact12@gmail.com>
2013-03-15 09:16:26 +0100

    premier commit

--- README.md
+++
@@ -1,4 +0,0 @@
-coUps
-=====
-
-Projet iaws - UE IAWS - Master 1 - 2012/2013
---
+++ src/main/java/iaws/NBMR/coUps/App.java
@@ -0,0 +1,13 @@
+package iaws.NBMR.coUps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
